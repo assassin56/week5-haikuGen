b52098038248c039f23709ad27ef352d310d955d
Joshua Hellman <jhell85@gmail.com>
2020-04-15 10:12:53 -0700

    updated tests and js

--- __tests__/haiku-checker.test.js
+++ __tests__/haiku-checker.test.js
@@ -1,7 +1,11 @@
-import { checkHaiku } from './../src/main.js'
+import { Haiku } from "./../src/main.js";
 
-describe("haiku checker", ()=>{
-  test('haiku checker returns boolean',  ()=>{
-    expect(typeof checkHaiku('poem')).toEqual('bool')
-  })
-})
+describe("haiku checker", () => {
+  beforeAll(()=>{
+      let line1 = "An old silent pond"
+      let line2 = "A frog jumps into the pond"
+      let line3 = "Splash! Silence again."
+      let haiku = new Haiku(line1, line2, line3)})
+
+
+});
--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import "./styles.css";
 import $ from "jquery";
 
-export const checkHaiku =(poem)=>{
-
-  return poem
+class Haiku {
+  constructor(line1, line2, line3) {
+    this.fistLine = line1;
+    this.secondLine = line2;
+    this.thirdLine = line3;
+  }
+  splitLines(line){
+      let lineArray = line.split(" ")
+      return lineArray
+  }
+  
 }
+
+
+// export const checkHaiku = (poem) => {
+//   if (poem.length > 17) {
+//     return false;
+//   }
+//   return true;
+// };
