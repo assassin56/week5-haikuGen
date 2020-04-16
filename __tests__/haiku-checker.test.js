6a735d9b2051494b3446c942d4ce6281c3684c3a
Joshua Hellman <jhell85@gmail.com>
2020-04-16 10:26:18 -0700

    added more test to cover full test sweets

--- __tests__/haiku-checker.test.js
+++ __tests__/haiku-checker.test.js
@@ -1,26 +1,30 @@
 import { Haiku } from "./../src/main.js";
 
 describe("haiku checker", () => {
+  let haiku = {};
+  beforeEach(() => { 
   let line1 = "An old silent pond";
   let line2 = "A frog jumps into the pond";
   let line3 = "Splash! Silence again.";
-  let haiku = new Haiku(line1, line2, line3);
+   haiku = new Haiku(line1, line2, line3); 
+})
+
   test("haiku method splitLines() are spliting the lines into arrays and returning them", () => {
-    expect(haiku.splitLines(line1)).toEqual(["An", "old", "silent", "pond"]);
+    expect(haiku.splitLines(haiku.firstLine)).toEqual(["An", "old", "silent", "pond"]);
   });
   test("haiku method verifyLines1and3() are returning true on a number of 5 or less", () => {
-    expect(haiku.verifyLines1and3(line1)).toBeTruthy();
+    expect(haiku.verifyLines1and3(haiku.firstLine)).toBeTruthy();
   });
   test("haiku method verifyLines1and3() are returning false on a number of 5 or more", () => {
-    line1 += " hello world";
-    expect(haiku.verifyLines1and3(line1)).toBeFalsy();
+    haiku.firstLine += " hello world";
+    expect(haiku.verifyLines1and3(haiku.firstLine)).toBeFalsy();
   });
   test("haiku method verifyLine2() are returning true on a number of 7 or less", () => {
-    expect(haiku.verifyLine2(line2)).toBeTruthy();
+    expect(haiku.verifyLine2(haiku.secondLine)).toBeTruthy();
   });
   test("haiku method verifyLine2() are returning false on a number of 7 or more", () => {
-    line2 += " hello world";
-    expect(haiku.verifyLine2(line2)).toBeFalsy();
+    haiku.secondLine += " hello world";
+    expect(haiku.verifyLine2(haiku.secondLine)).toBeFalsy();
   });
   test("haiku method countSyllables() is counting the vowels of the word passed into it", () => {
     expect(haiku.countSyllables("hello")).toEqual(2);
@@ -34,10 +38,10 @@ describe("haiku checker", () => {
   test("haiku method countSyllables() is only counting for 1 syllable if a word ends in 'le' or 'les' and the letter before 'l' is a constant", ()=> {
     expect(haiku.countSyllables("automobiles")).toEqual(4);
   })
-  // test("haiku method returnHaiku() is returning false on lines with correct line lengths but incorrect syllabol lengths", ()=> {
-  //   haiku.firstLine += " ACQUAINTANCE"
-  //   expect(haiku.returnHaiku()).toBeTruthy();
-  // })
+  test("haiku method returnHaiku() is returning false on lines with correct line lengths but incorrect syllabol lengths", ()=> {
+    haiku.firstLine += " ACQUAINTANCE"
+    expect(haiku.returnHaiku()).toBeFalsy();
+  })
   test("haiku method returnHaiku() is returning true on Hakiu lines with correct syllabols in each line", ()=> {
     expect(haiku.returnHaiku()).toBeTruthy();
   })
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styles.css";
+//import "./styles.css";
 import $ from "jquery";
 
 export class Haiku {
@@ -29,30 +29,34 @@ export class Haiku {
     let vowelArr = ["a", "e", "i", "o", "u", "y"];
     str = str.toLowerCase();
     let counter = 0;
-    for (let i = 0; i < str.length; i++) {
-      if (vowelArr.includes(str[i])) {
-        // checking if we are at the end of the word when we find  a vowel then see if it's an "e"
-        if (i + 1 === str.length) {
-          if (str[i] != "e") {
+    if (str.length <= 3) {
+      counter = 1;
+    } else {
+      for (let i = 0; i < str.length; i++) {
+        if (vowelArr.includes(str[i])) {
+          // checking if we are at the end of the word when we find  a vowel then see if it's an "e"
+          if (i + 1 === str.length) {
+            if (str[i] != "e") {
+              counter++;
+            }
+          } else if (
+            vowelArr.includes(str[i + 1]) &&
+            vowelArr.includes(str[i + 2])
+          ) {
+            counter++;
+            i += 2;
+          } else if (vowelArr.includes(str[i + 1])) {
+            counter++;
+            i++;
+          } else if (
+            str.length === i + 2 &&
+            str[i] === "e" &&
+            str[i - 1] === "l" &&
+            vowelArr.includes(str[i - 2])
+          ) {
+          } else {
             counter++;
           }
-        } else if (
-          vowelArr.includes(str[i + 1]) &&
-          vowelArr.includes(str[i + 2])
-        ) {
-          counter++;
-          i += 2;
-        } else if (vowelArr.includes(str[i + 1])) {
-          counter++;
-          i++;
-        } else if (
-          str.length === i + 2 &&
-          str[i] === "e" &&
-          str[i - 1] === "l" &&
-          vowelArr.includes(str[i - 2])
-        ) {
-        } else {
-          counter++;
         }
       }
     }
@@ -64,16 +68,33 @@ export class Haiku {
       this.verifyLines1and3(this.thirdLine) &&
       this.verifyLine2(this.secondLine)
     ) {
-      // let line1Array = this.splitLines(this.firstLine);
-      // let line2Array = this.splitLines(this.secondLine);
-      // let line3Array = this.splitLines(this.thirdLine);
-      // if (
-      //   this.verifyLines1and3(line1Array) &&
-      //   this.verifyLine2(line2Array) &&
-      //   this.verifyLines1and3(line3Array)
-      // ) {
+      let line1Array = this.splitLines(this.firstLine);
+      let line2Array = this.splitLines(this.secondLine);
+      let line3Array = this.splitLines(this.thirdLine);
+      let line1Syllables = 0;
+      let line2Syllables = 0;
+      let line3Syllables = 0;
+      line1Array.forEach((word) => {
+        line1Syllables += this.countSyllables(word);
+      });
+      line2Array.forEach((word) => {
+        line2Syllables += this.countSyllables(word);
+      });
+      line3Array.forEach((word) => {
+        line3Syllables += this.countSyllables(word);
+      });
+
+      if (
+        line1Syllables === 5 &&
+        line3Syllables === 5 &&
+        line2Syllables === 7
+      ) {
         return true;
-        // }
-    } else return false;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 }
